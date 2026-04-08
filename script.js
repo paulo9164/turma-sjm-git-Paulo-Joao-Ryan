693cb4fe311d4b594c329e6f2b5f1dfecd5ba909
JoaoFerreira311 <joaopedroferreira787@gmail.com>
2026-04-08 09:46:12 +0100

    verifica se o utilizador colocou nome e telefone e vai buscá-los à lista

--- script.js
+++ script.js
@@ -2,3 +2,6 @@
 function adicionarContacto() {
     let nome = prompt("Nome do contacto:");
     let telefone = prompt("Telefone:");
+
+    if (nome && telefone) {
+        let lista = document.getElementById("lista");
